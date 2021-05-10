7487b2170c7b268b8182b62c36ebf6a79b88ff5a
Alexandra <alex@faller.us>
2021-05-10 06:45:53 +0100

    added event listeners, got the dom elements

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const charAmountRange = document.getElementById('charAmountRange');
 const charAmountNumber = document.getElementById('charAmountNumber');
+const form = document.getElementById('passwordGeneratorForm');
+const includeUpperEl = document.getElementById('includeUppercase');
+const includeNumEl = document.getElementById('includeNumbers');
+const includeSymbEl = document.getElementById('includeSymbols');
 
+//sync the slider and the number box to show the same
 charAmountNumber.addEventListener('input', syncCharAmount);
 charAmountRange.addEventListener('input', syncCharAmount);
 
@@ -9,3 +14,23 @@ function syncCharAmount(e) {
 	charAmountNumber.value = value;
 	charAmountRange.value = value;
 }
+
+form.addEventListener('submit', (e) => {
+	e.preventDefault.default();
+	const charAmount = charAmountNumber.value;
+	const includeUpper = includeUpperEl.checked;
+	const includeNum = includeNumEl.checked;
+	const includeSymb = includeSymbEl.checked;
+
+	const password = generatePassword(charAmount, includeUpper, includeNum, includeSymb);
+});
+
+function arrayFromLowToHigh(low, high) {
+	for (let i = low; i < high; i++) {
+		array.push(i);
+	}
+	return array;
+}
+
+//use the ascii char codes to generate the random letters, nums and symbols
+function generatePassword(charAmount, includeUpper, includeNum, includeSymb) {}
